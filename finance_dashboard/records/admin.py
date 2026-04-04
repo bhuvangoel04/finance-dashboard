f2fd11db74fdff93328704062a4978be8be58c33
Bhuvan Goel <bhuvangoel2004@gmail.com>
2026-04-04 23:03:40 +0530

    Admin app registration and management

--- finance_dashboard/records/admin.py
+++ finance_dashboard/records/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import FinancialRecord
 
-# Register your models here.
+
+@admin.register(FinancialRecord)
+class FinancialRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'record_type', 'category', 'amount', 'date', 'created_by', 'created_at')
+    list_filter = ('record_type', 'category', 'date')
+    search_fields = ('description', 'notes')
+    ordering = ('-date',)
+    readonly_fields = ('created_at', 'updated_at', 'created_by')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
